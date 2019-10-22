32163efa72fc71a334809a8a4e866a275855467b
anwar156 <azizanwar@mail.ugm.ac.id>
2019-10-22 16:32:18 +0700

    menambah functions register

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function koneksi(){
+
     // koneksi ke database
     $conn = mysqli_connect("localhost", "clevo", "12345678") or die("Koneksi ke database GAGAL!");
 
@@ -9,6 +10,18 @@ function koneksi(){
     return $conn;
 }
 
+function register($conn, $post = array()){
+    
+    // query sql 
+    $query = sprintf("INSERT INTO user (user_login, user_email, user_pass, display_name)
+        VALUES ('%s', '%s', '%s', '%s')", $post["username"], $post["user_email"], $post["user_pass"], $post["name"]);
+
+    // execute query
+    mysqli_query($conn, $query);
+
+    // mengembalikkan jumlah baris yang terpengaruhi
+    return mysqli_affected_row($conn)
+}
 
 
 ?>
