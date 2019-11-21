89d6bf58834efedd715bd51b7ee74289988beb87
Aziz Anwar <azizanwar@mail.ugm.ac.id>
2019-11-21 23:03:17 +0700

    update database

--- database/djogjaEvent.sql
+++ database/djogjaEvent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 19, 2019 at 02:16 PM
+-- Generation Time: Nov 21, 2019 at 04:01 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.11
 
@@ -25,11 +25,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `event_id` bigint(20) UNSIGNED NOT NULL,
+  `event_author` bigint(20) UNSIGNED NOT NULL,
+  `event_post` datetime NOT NULL DEFAULT current_timestamp(),
+  `event_tittle` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `event_desc` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `event_location` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `event_datetime` datetime NOT NULL,
+  `event_participant` int(11) NOT NULL,
+  `event_image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `event_register` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 === tidak',
+  `event_status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1 === post, 0 === delete'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `user_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
   `user_username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -43,11 +63,19 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `user_username`, `user_email`, `user_pass`, `user_register`, `user_profile`, `user_displayname`) VALUES
-(1, 'anwar156', 'azizanwar@mail.ugm.ac.id', '$2y$10$njdD5N39ntK8CY1I1tZ4ieWZfhrMvVk2pdwh.FjjTHIOvJeLqj4ki', '2019-11-19', NULL, 'azis anwar');
+(1, 'anwar156', 'azizanwar@mail.ugm.ac.id', '$2y$10$njdD5N39ntK8CY1I1tZ4ieWZfhrMvVk2pdwh.FjjTHIOvJeLqj4ki', '2019-11-19', NULL, 'azis anwar'),
+(2, 'test1', 'test1@test1.test1', '$2y$10$aXKYta86y/cvmE3g7ag3zusp2FuczuhcN6r.mGJjbo2ChAqrox2GC', '2019-11-20', NULL, 'test1');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`event_id`),
+  ADD KEY `user-make-event` (`event_author`);
 
 --
 -- Indexes for table `user`
@@ -62,10 +90,26 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `event_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `event`
+--
+ALTER TABLE `event`
+  ADD CONSTRAINT `user-make-event` FOREIGN KEY (`event_author`) REFERENCES `user` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
